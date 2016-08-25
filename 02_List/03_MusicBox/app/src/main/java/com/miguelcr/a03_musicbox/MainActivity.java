330a960c05ff075e4a45f6ed60cd57610f080ee7
Miguel Campos <camposmiguel@gmail.com>
2016-08-25 12:16:14 +0200

    MusicBox 2

--- 02_List/03_MusicBox/app/src/main/java/com/miguelcr/a03_musicbox/MainActivity.java
+++ 02_List/03_MusicBox/app/src/main/java/com/miguelcr/a03_musicbox/MainActivity.java
@@ -1,5 +1,7 @@
 package com.miguelcr.a03_musicbox;
 
+import android.media.AudioAttributes;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.GridView;
@@ -11,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     private GridView lista;
     private List<MusicItem> instruments;
     private MusicAdapter adapter;
+    SoundPool soundPool;
+    int soundGuitar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +33,22 @@ public class MainActivity extends AppCompatActivity {
 
         adapter = new MusicAdapter(MainActivity.this,
                 R.layout.music_item,instruments);
+
+        lista.setAdapter(adapter);
+
+        // Player properties
+        AudioAttributes aa = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_ALARM)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build();
+
+        soundPool = new SoundPool.Builder()
+                .setMaxStreams(10)
+                .setAudioAttributes(aa)
+                .build();
+
+        // Load sound of guitar
+        soundGuitar = soundPool.load(this,R.raw.guitar_sound,1);
 
 
     }
