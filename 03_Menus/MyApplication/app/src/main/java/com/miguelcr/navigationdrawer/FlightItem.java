65fe5c11e4e06ea8451926f553426f270bf179db
Miguel Campos <camposmiguel@gmail.com>
2016-08-29 09:17:00 +0200

    DuckHunt Resources

---
+++ 03_Menus/MyApplication/app/src/main/java/com/miguelcr/navigationdrawer/FlightItem.java
@@ -0,0 +1,14 @@
+package com.miguelcr.navigationdrawer;
+
+/**
+ * Created by miguelcampos on 26/8/16.
+ */
+public class FlightItem {
+    private int iconCompany;
+    private String period;
+    private String source;
+    private String time;
+    private String link;
+
+
+}
